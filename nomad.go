9bfff1238c822e84507b7411d9768542c9638123
Mike Nomitch <mail@mikenomitch.com>
2022-11-18 12:41:31 -0800

    Set up decently with todos and tests

--- nomad.go
+++ nomad.go
@@ -2,10 +2,11 @@ package nomad
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/coredns/coredns/plugin"
-	"github.com/coredns/coredns/plugin/metrics"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
+	"github.com/coredns/coredns/request"
 	nomad "github.com/hashicorp/nomad/api"
 
 	"github.com/miekg/dns"
@@ -26,39 +27,26 @@ type Nomad struct {
 // ServeDNS implements the plugin.Handler interface. This method gets called when example is used
 // in a Server.
 func (n Nomad) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
-	// a slightly more complex example as to make this more interesting.
-	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
-	// answer comes back, it will print "example".
+	state := request.Request{W: w, Req: r}
+	qname := state.Name()
 
-	// Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
-	log.Debug("Received response")
+	fmt.Println("qname:")
+	fmt.Println(qname)
 
-	// Wrap.
-	pw := NewResponsePrinter(w)
+	// TODO A: GET A STUBBED OUT REPLY WORKING WITH FAKE DATA PASSING 1ST TEST
 
-	// Export metric with the server label set to the current server handling the request.
-	requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
+	// TODO B: GET NOMAD RUNNING WITH THE A JOB TO MATCH
 
-	// Call next plugin (if any).
-	return plugin.NextOrFailure(n.Name(), n.Next, ctx, pw, r)
+	// TODO C: MAKE THIS TEST MAKE A REQUEST TO NOMAD FOR THE SD INFO
+
+	// TODO D: PASS THE SD INFO INTO THE RESPONSE & UPDATE THE TEST
+
+	m := new(dns.Msg)
+	m.SetReply(r)
+
+	w.WriteMsg(m)
+	return dns.RcodeSuccess, nil
 }
 
 // Name implements the Handler interface.
-func (n Nomad) Name() string { return "example" }
-
-// ResponsePrinter wrap a dns.ResponseWriter and will write example to standard output when WriteMsg is called.
-type ResponsePrinter struct {
-	dns.ResponseWriter
-}
-
-// NewResponsePrinter returns ResponseWriter.
-func NewResponsePrinter(w dns.ResponseWriter) *ResponsePrinter {
-	return &ResponsePrinter{ResponseWriter: w}
-}
-
-// WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard output.
-func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
-	log.Info("nomad")
-	return r.ResponseWriter.WriteMsg(res)
-}
+func (n Nomad) Name() string { return "nomad" }
